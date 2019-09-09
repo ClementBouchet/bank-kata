83cd5872087857c3264d7e9aefc92c8e4da7c1bb
ClementBouchet <bouchetclem@gmail.com>
2019-09-09 11:44:11 +0200

    renaming in test method

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -7,10 +7,10 @@ import java.math.BigDecimal;
 public class BankAccountTest {
 
     @Test
-    public void a_bank_client_makes_a_deposit() {
+    public void when_a_bank_client_makes_a_deposit_in_an_empty_account_then_the_account_balance_is_the_amount_deposited() {
         BigDecimal expectedAccountBalance = BigDecimal.valueOf(10.19);
-        BigDecimal accountBalance = BigDecimal.ZERO;
-        Account account = new Account(accountBalance);
+        BigDecimal initialAccountBalance = BigDecimal.ZERO;
+        Account account = new Account(initialAccountBalance);
         BigDecimal amountDeposited = BigDecimal.valueOf(10.19);
 
         account.deposit(amountDeposited);
