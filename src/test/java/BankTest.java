f567ae786c2ffe3201db0229ccb3ac1cef9a2dce
ClementBouchet <bouchetclem@gmail.com>
2019-09-13 15:53:45 +0200

    Refacto Bank tests

--- src/test/java/BankTest.java
+++ src/test/java/BankTest.java
@@ -17,18 +17,18 @@ import java.util.List;
 public class BankTest {
 
     @Test
-    public void when_make_deposit_then_do_deposit() {
+    public void when_make_deposit_then_create_history_line_of_operation() {
         List<HistoryLine> historyLines = new ArrayList<>();
         LocalDateTime operationDate = LocalDateTime.of(2019, 1, 1, 12, 0);
         Amount operationAmount = Amount.of(BigDecimal.valueOf(20));
-        Amount accountBalance = Amount.of(BigDecimal.valueOf(20));
+        Amount accountBalanceAfterOperation = Amount.of(BigDecimal.valueOf(20));
 
         TimeProvider mockedTimeProvider = Mockito.mock(TimeProvider.class);
         Mockito.when(mockedTimeProvider.now()).thenReturn(operationDate);
 
         Account account = new Account(Amount.of(BigDecimal.ZERO), new AccountHistory(new ArrayList<>()));
 
-        historyLines.add(new HistoryLine(OperationType.DEPOSIT, operationDate, operationAmount, accountBalance));
+        historyLines.add(new HistoryLine(OperationType.DEPOSIT, operationDate, operationAmount, accountBalanceAfterOperation));
         AccountHistory expectedHistory = new AccountHistory(historyLines);
 
         Bank bank = new Bank(mockedTimeProvider);
@@ -41,18 +41,18 @@ public class BankTest {
     }
 
     @Test
-    public void when_withdraw_then_do_withdrawal_in_account() {
+    public void when_withdraw_then_create_history_line_of_operation() {
         List<HistoryLine> historyLines = new ArrayList<>();
         LocalDateTime operationDate = LocalDateTime.of(2019, 1, 1, 12, 0);
         Amount operationAmount = Amount.of(BigDecimal.valueOf(20));
-        Amount accountBalance = Amount.of(BigDecimal.valueOf(0));
+        Amount accountBalanceAfterOperation = Amount.of(BigDecimal.valueOf(0));
 
         TimeProvider mockedTimeProvider = Mockito.mock(TimeProvider.class);
         Mockito.when(mockedTimeProvider.now()).thenReturn(operationDate);
 
         Account account = new Account(Amount.of(BigDecimal.valueOf(20)), new AccountHistory(new ArrayList<>()));
 
-        historyLines.add(new HistoryLine(OperationType.WITHDRAWAL, operationDate, operationAmount, accountBalance));
+        historyLines.add(new HistoryLine(OperationType.WITHDRAWAL, operationDate, operationAmount, accountBalanceAfterOperation));
         AccountHistory expectedHistory = new AccountHistory(historyLines);
 
         Bank bank = new Bank(mockedTimeProvider);
